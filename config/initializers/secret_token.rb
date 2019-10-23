8bcc94ea3a13b8afeeda350b1dfee921d328ff7c
Masaki Hara <ackie.h.gmai@gmail.com>
2019-10-23 15:42:31 +0900

    bundle exec rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myapp::Application.config.secret_token = '0677fe185d3afa10afaf18a6629c880a8dd1a6058db30a9b24c2f45ea42a8c0d1cdec125d932b40fad71fc8a13bbec0e9c90fa848b08731b78c2f57e90bfb447'
+Myapp::Application.config.secret_token = 'd97dc23d770fa2d5812e46ad28e179963b4a428273867c6900bb47b3f47e956c58491f53078484a751b1b52dfc0caa8ae0366101967c06d4ffba3e112401faa7'
