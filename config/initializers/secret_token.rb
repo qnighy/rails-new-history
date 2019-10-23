ebe035866f193ec53ad22a93623235fb43f3f9df
Masaki Hara <ackie.h.gmai@gmail.com>
2019-10-23 15:47:47 +0900

    bundle exec rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myapp::Application.config.secret_token = '3075b4971973b4021d83ee5eb2e7254a84ae5af1ba7659b40523b2b12ae5e586c3cd975618572843fd2fef3f171e60258e52e9f72c4b459f74096e0307244173'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '107f8f863d751981f01b6915cf455bb7519da299c210c4feae3571d0d626f7142bff0ce1797c661d90b65f054bd925773bbecf8fa43dc961177f9ea32757141f'
